61d6d0bf6a4ffcba17680a2d4fae4f1106b2eef4
wiKanok <kanokwan1401.ks@gmail.com>
2024-07-03 12:06:58 +0700

    Nested Loop (Exercise)/wi-Complete

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,25 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+//เริ่มเขียนโค้ดตรงนี้;
+function printStar(n) {
+  let finalResult = "";
+  for (let i = 1; i <= n; i++) {
+    let rowResult = "";
+    for (let j = 0; j < i; j++) {
+      rowResult += "*" + "\t";
+    }
+    finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+
+console.log(printStar(5));
+
+// Solution 2
+// function printStar(n) {
+//   let finalResult = "";
+//   for (let i = 1; i <= n; i++) {
+//     finalResult += "*\t".repeat(i) + "\n";
+//   }
+//   return finalResult;
+// }
+
+// console.log(printStar(5));
